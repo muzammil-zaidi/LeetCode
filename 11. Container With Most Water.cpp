8542e0829ba5734f7d374810318062f2b0451264
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-08-07 03:24:26 +0500

    Update 11. Container With Most Water.cpp

--- 11. Container With Most Water.cpp
+++ 11. Container With Most Water.cpp
@@ -1,1 +1,16 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int maxwater=0;
+        for(int i=0;i<height.size();i++){
+            for(int j=i+1;j<height.size();j++){
+                int w=j-i;
+                int ht=min(height[i],height[j]);
+                int currwater=w*ht;
 
+                maxwater=max(maxwater,currwater);
+            }
+        }
+        return maxwater;
+    }
+};
