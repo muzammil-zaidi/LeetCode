1dfdb773919f38757a1f1abc0d17a58d0184c27a
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-08-11 20:29:16 +0500

    Update 2. Add Two Numbers.cpp

--- 2. Add Two Numbers.cpp
+++ 2. Add Two Numbers.cpp
@@ -1,1 +1,40 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* current = &dummy;
+        int carry = 0;
 
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int val1, val2;
+
+            if (l1 != nullptr) {
+                val1 = l1->val;
+            } else {
+                val1 = 0;
+            }
+
+            if (l2 != nullptr) {
+                val2 = l2->val;
+            } else {
+                val2 = 0;
+            }
+
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+
+            current->next = new ListNode(sum % 10);
+            current = current->next;
+
+            if (l1 != nullptr) {
+                l1 = l1->next;
+            }
+
+            if (l2 != nullptr) {
+                l2 = l2->next;
+            }
+        }
+
+        return dummy.next;
+    }
+};
